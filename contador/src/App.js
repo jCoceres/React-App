54f49d07fb7b33166d476e5d226097cc75c047a2
Jennifer <jennicer.c17@gmail.com>
2022-04-15 16:11:20 -0300

    cambie el orden de las funciones

--- contador/src/App.js
+++ contador/src/App.js
@@ -5,16 +5,14 @@ import React, { useState } from 'react';
 function Contador () {
   const [count, setCount] = useState(0);
   const suma = ()=> setCount(count +1);
-  const resta = ()=> setCount(count -1);
-  const reset = ()=> setCount(0);
   
   return (
     <div className='text-center mt-4'>
       <h1>CONTADOR</h1>
       <p className='fs-4'>{count}</p>
       <button onClick={suma} className='btn btn-success btn-lg me-3'>Agregar</button>
-      <button onClick={resta} className='btn btn-success btn-lg me-3'>Quitar</button>
-      <button onClick={reset} className='btn btn-success btn-lg'>Reset</button>
+      <button onClick={()=> setCount(count -1)} className='btn btn-success btn-lg me-3'>Quitar</button>
+      <button onClick={()=> setCount(0)} className='btn btn-success btn-lg'>Reset</button>
     </div>
   );
 }
